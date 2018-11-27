edb91701e98d498405c2db49a387c011e8fc7125
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2018-11-27 13:53:37 +0100

    Base SelectableFuture on CompletableFuture and remove SimpleFuture.

--- src/main/java/no/ssb/concurrent/futureselector/SelectableFuture.java
+++ src/main/java/no/ssb/concurrent/futureselector/SelectableFuture.java
@@ -1,27 +1,13 @@
 package no.ssb.concurrent.futureselector;
 
-import java.util.Collection;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.RunnableFuture;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-public class SelectableFuture<V> extends SimpleFuture<V> implements RunnableFuture<V> {
-
-    private static class DoneQueueState {
-        private final AtomicBoolean signalled = new AtomicBoolean(false);
-        private final BlockingQueue<Selection> doneQueue;
-        private final Selection selection;
-
-        private DoneQueueState(BlockingQueue<Selection> doneQueue, Selection selection) {
-            this.doneQueue = doneQueue;
-            this.selection = selection;
-        }
-    }
+public class SelectableFuture<V> extends CompletableFuture<V> implements RunnableFuture<V> {
 
     private final Callable<V> wrap;
-    private final Collection<DoneQueueState> doneQueues = new CopyOnWriteArrayList<>();
 
     public SelectableFuture(Runnable runnable, V value) {
         wrap = () -> {
@@ -36,39 +22,19 @@ public class SelectableFuture<V> extends SimpleFuture<V> implements RunnableFutu
 
     @Override
     public void run() {
-        worker(Thread.currentThread());
         try {
             complete(wrap.call());
-        } catch (Exception e) {
-            executionException(e);
-        } finally {
-            clearWorker();
+        } catch (Throwable t) {
+            completeExceptionally(t);
         }
     }
 
     SelectableFuture<V> registerWithDoneQueueAndMarkSelectableIfDone(BlockingQueue<Selection> doneQueue, Selection selection) {
-        DoneQueueState state = new DoneQueueState(doneQueue, selection);
-        doneQueues.add(state);
-        if (isDone()) {
-            if (state.signalled.compareAndSet(false, true)) {
-                if (!state.doneQueue.offer(selection)) {
-                    throw new IllegalStateException("Unable to offer this Future instance to doneQueue.");
-                }
+        thenAccept(v -> {
+            if (!doneQueue.offer(selection)) {
+                throw new IllegalStateException("Unable to offer this Future instance to doneQueue.");
             }
-        }
+        });
         return this;
-    }
-
-    @Override
-    SimpleFuture<V> markDone() {
-        SimpleFuture<V> result = super.markDone();
-        for (DoneQueueState state : doneQueues) {
-            if (state.signalled.compareAndSet(false, true)) {
-                if (!state.doneQueue.offer(state.selection)) {
-                    throw new IllegalStateException("Unable to offer this Future instance to doneQueue.");
-                }
-            }
-        }
-        return result;
     }
 }
